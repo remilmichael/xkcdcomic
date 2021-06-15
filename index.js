17224ba992b2ea24582d82a1cdf1f654f7158e66
Remil Michael <mail@remil.me>
2021-06-15 19:24:55 -0400

    add more styles and features

--- index.js
+++ index.js
@@ -87,6 +87,21 @@ const updateAndWriteCount = () => {
     localStorage.setItem('count', JSON.stringify(Array.from(countMap.entries())))
 }
 
+const getMonthName = (monthNumber) => {
+    if (monthNumber === '1') return 'January'
+    if (monthNumber === '2') return 'February'
+    if (monthNumber === '3') return 'March'
+    if (monthNumber === '4') return 'April'
+    if (monthNumber === '5') return 'May'
+    if (monthNumber === '6') return 'June'
+    if (monthNumber === '7') return 'July'
+    if (monthNumber === '8') return 'August'
+    if (monthNumber === '9') return 'September'
+    if (monthNumber === '10') return 'October'
+    if (monthNumber === '11') return 'November'
+    if (monthNumber === '12') return 'December'
+}
+
 const urlSearchParams = new URLSearchParams(window.location.search)
 currentComicNumber = urlSearchParams.get('comic')
 
@@ -102,8 +117,14 @@ btnRandom.addEventListener('click', getRandom)
 if (currentComicNumber) {
     fetchComics(currentComicNumber)
         .then(response => {
-            document.getElementById("comicImg").src = response.img
-            document.getElementById("comicImg").style.display = 'block'
+            const monthName = getMonthName(response.month)
+
+            document.getElementById('comicImg').src = response.img
+            document.getElementById('comicImg').alt = response.alt
+            document.getElementById('comicImg').style.display = 'block'
+            document.getElementById('comicTitle').append(document.createTextNode(response.title))
+            document.getElementById('comicDate').append(document.createTextNode(`${monthName} ${response.day}, ${response.year}`))
+
             updateAndWriteCount()
         })
         .catch(err => {
